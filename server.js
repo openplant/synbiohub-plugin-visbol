1bc2d4c487093436509e608c9f8f5dd737fa0db1
benjhatch <benjamin.hat4@gmail.com>
2021-11-03 20:12:44 -0600

    added evaluate endpoint

--- server.js
+++ server.js
@@ -16,4 +16,13 @@ app.get('/Status', (req, res) => {
    res.status(200).send("VisBOL plugin up and running");
 });
 
+// ACCESS: PUBLIC
+// Route = /Evaluate
+app.post('/Evaluate', (req, res) => {
+   const type = req.body.type;
+   console.log(`Evaluating ${type}`);
+   res.status(200).send("Type is compatible with the VisBOL plugin");
+})
+
+// start server
 app.listen(port, () => console.log(`VisBOL plugin listening at http://${address}:${port}`));
