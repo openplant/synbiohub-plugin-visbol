6902e1fdd368049c3b40b4d98ec80b38f60bce0d
benjhatch <benjamin.hat4@gmail.com>
2022-01-05 11:41:01 -0800

    added console logs for debugging

--- src/index.js
+++ src/index.js
@@ -11,16 +11,21 @@ function* iterate_object(o) {
       yield [keys[i], o[keys[i]]];
    }
 }
+
+console.log("iterating over initial state");
 const data = {};
 for (var [key, val] of iterate_object(window.__INITIAL_STATE__)) {
    data[key] = val;
 }
 
 if (data.svg) {
+   console.log("rendering svg");
    render(<Renderer svg={data.svg} width={data.width} height={data.height} />, document.getElementById("plugin-visual-visbol"));
 }
 else {
+   console.log("preparing display");
    const display = prepareDisplay(data.displayList);
 
+   console.log("rendering visbol");
    render(<Renderer display={display} />, document.getElementById("plugin-visual-visbol"));
 }
