18910aa89c5461326fd9197af77060874512ef0e
sgabenza <sgabenza86@gmail.com>
2023-09-26 16:40:40 +0200

    [data] fetch xml

--- localserver.js
+++ localserver.js
@@ -1,12 +1,16 @@
-const express = require('express');
-const { createRendering, createError, createSVG } = require('./templates.js');
-const { getSBOLFromUrl, convertSBOLtoMxGraph, mxGraphToSVG } = require('./tools.js');
-const { createDisplay, prepareDisplay } = require('visbol');
+const express = require("express");
+const { createRendering, createError, createSVG } = require("./templates.js");
+const {
+  getSBOLFromUrl,
+  convertSBOLtoMxGraph,
+  mxGraphToSVG,
+} = require("./tools.js");
+const { createDisplay, prepareDisplay } = require("visbol");
 const path = require("path");
 
 // set up server
 const app = express();
-const port = 5000;
+const port = 5003;
 const address = "localhost";
 
 app.use(express.json());
@@ -14,9 +18,9 @@ app.use(express.json());
 // ACCESS: PUBLIC
 // Route = /Status
 // Returns whether plugin is up and running
-app.get('/Status', (req, res) => {
-   console.log('Status checked');
-   res.status(200).send("VisBOL plugin up and running");
+app.get("/Status", (req, res) => {
+  console.log("Status checked");
+  res.status(200).send("VisBOL plugin up and running");
 });
 
 // ACCESS: PUBLIC
@@ -24,64 +28,108 @@ app.get('/Status', (req, res) => {
 // Returns whether type is compatible with plugin
 // (this needs to be developed more, don't know all
 // the types VisBOL is compatible with)
-app.post('/Evaluate', (req, res) => {
-   const type = req.body.type;
-   console.log(`Evaluating ${type}`);
-   if (type == 'Component' || type == 'ComponentDefinition' || type == 'Layout')
-      res.status(200).send(`The type ${type} is compatible with the VisBOL plugin`);
-   else
-      res.status(415).send(`The type ${type} is NOT compatible with the VisBOL plugin`);
+app.post("/Evaluate", (req, res) => {
+  const type = req.body.type;
+  console.log(`Evaluating ${type}`);
+  if (type == "Component" || type == "ComponentDefinition" || type == "Layout")
+    res
+      .status(200)
+      .send(`The type ${type} is compatible with the VisBOL plugin`);
+  else
+    res
+      .status(415)
+      .send(`The type ${type} is NOT compatible with the VisBOL plugin`);
+});
+
+app.get("/RunLocal", async (req, res) => {
+  // let url = "https://synbiohub.org/public/SEGA/SEGA008/1/sbol";
+  let url = "https://synbiohub.org/public/igem/BBa_K1033221/1/sbol";
+  const type = "";
+  const hostAddress = req.get("host");
+  // uncomment the following 2 lines if running the plugin locally
+  if (type === "Layout")
+    // for local dev+
+    url = url.replace("https://synbiohub.org", "http://localhost:7777");
+  console.log(`Run url=${url} : host address=${hostAddress}`);
+
+  try {
+    const sbol = await getSBOLFromUrl(url);
+    if (type !== "Layout") {
+      const displayList = await createDisplay(sbol);
+      const display = prepareDisplay(displayList);
+      display.renderGlyphs();
+
+      const properties = {
+        display,
+      };
+      res.send(createRendering(properties, hostAddress));
+    } else {
+      const mxGraph = await convertSBOLtoMxGraph(sbol);
+      const svg = mxGraphToSVG(mxGraph);
+      const properties = {
+        svg: svg.xml,
+        width: svg.width,
+        height: svg.height,
+      };
+      const createdSVG = createSVG(properties, hostAddress);
+      res.send(createSVG(properties, hostAddress));
+    }
+  } catch (error) {
+    res.send(createError(error));
+  }
 });
 
 // ACCESS: PUBLIC
 // Route = /Run
 // Returns html for VisBOL rendering
 // in a web application
-app.post('/Run', async (req, res) => {
-   let url = req.body.complete_sbol;
-   const type = req.body.type;
-   const hostAddress = req.get("host");
-   // uncomment the following 2 lines if running the plugin locally
-   if (type === 'Layout') //for local dev
-       url = url.replace('https://synbiohub.org', 'http://localhost:7777');
-   console.log(`Run url=${url} : host address=${hostAddress}`);
-   try {
-      const sbol = await getSBOLFromUrl(url);
-      if (type !== 'Layout') {
-         const displayList = await createDisplay(sbol);
-         const display = prepareDisplay(displayList);
-         display.renderGlyphs();
+app.post("/Run", async (req, res) => {
+  let url = req.body.complete_sbol;
+  const type = req.body.type;
+  const hostAddress = req.get("host");
+  // uncomment the following 2 lines if running the plugin locally
+  //   if (type === "Layout")
+  //for local dev+
+  //  url = url.replace("https://synbiohub.org", "http://localhost:7777");
+  console.log(`Run url=${url} : host address=${hostAddress}`);
 
-         const properties = {
-            display
-         }
-         
-         res.send(createRendering(properties, hostAddress));
-      } else {
-         const mxGraph = await convertSBOLtoMxGraph(sbol);
-         const svg = mxGraphToSVG(mxGraph);
+  try {
+    const sbol = await getSBOLFromUrl(url);
+    if (type !== "Layout") {
+      const displayList = await createDisplay(sbol);
+      const display = prepareDisplay(displayList);
+      display.renderGlyphs();
 
-         const properties = {
-            svg: svg.xml,
-            width: svg.width,
-            height: svg.height
-         }
-         res.send(createSVG(properties, hostAddress));
-      }
-   }
-   catch (error) {
-      res.send(createError(error));
-   }
+      const properties = {
+        display,
+      };
+      res.send(createRendering(properties, hostAddress));
+    } else {
+      const mxGraph = await convertSBOLtoMxGraph(sbol);
+      const svg = mxGraphToSVG(mxGraph);
+      const properties = {
+        svg: svg.xml,
+        width: svg.width,
+        height: svg.height,
+      };
+      const createdSVG = createSVG(properties, hostAddress);
+      res.send(createSVG(properties, hostAddress));
+    }
+  } catch (error) {
+    res.send(createError(error));
+  }
 });
 
 // ACCESS: PUBLIC
 // Route = /visbol.js
 // Gets built rendering file to be sent
 // to browser
-app.get('/visbol.js', (req, res) => {
-   const __dirname = path.resolve();
-   res.sendFile(path.join(__dirname, 'dist', 'main.js'));
+app.get("/visbol.js", (req, res) => {
+  const __dirname = path.resolve();
+  res.sendFile(path.join(__dirname, "dist", "main.js"));
 });
 
 // start server
-app.listen(port, () => console.log(`VisBOL plugin listening at http://${address}:${port}`));
+app.listen(port, () =>
+  console.log(`VisBOL plugin listening at http://${address}:${port}`)
+);
--- templates.js
+++ templates.js
@@ -1,45 +1,46 @@
 const serialize = require("serialize-javascript");
 
 module.exports = {
-   // returning the visbol rendering
-   createRendering: (properties, hostAddress) => {
-      const content =
-      `
-      <script type="text/javascript">window.__INITIAL_STATE__ = ${serialize(properties)}</script>
-      <script type="text/javascript" src="https://${hostAddress}/visbol.js" charset="utf-8"></script>
+  // returning the visbol rendering
+  createRendering: (properties, hostAddress) => {
+    const content = `
+      <script type="text/javascript">window.__INITIAL_STATE__ = ${serialize(
+        properties
+      )}</script>
+      <script type="text/javascript" src="http://${hostAddress}/visbol.js" charset="utf-8"></script>
       `;
 
-      return populateTemplate(content);
-   },
+    return populateTemplate(content);
+  },
 
-   // returning an error
-   createError: (error) => {
-      const content =
-      `
+  // returning an error
+  createError: (error) => {
+    const content = `
       Error while parsing the file for VisBOL rendering:
       ${error}
       `;
 
-      return populateTemplate(content);
-   },
+    return populateTemplate(content);
+  },
 
-   createSVG: (properties, hostAddress) => {
-      const content =
-      `
-      <script type="text/javascript">window.__INITIAL_STATE__ = ${serialize(properties)}</script>
+  createSVG: (properties, hostAddress) => {
+    const content = `
+      <script type="text/javascript">window.__INITIAL_STATE__ = ${serialize(
+        properties
+      )}</script>
       <script type="text/javascript" src="https://${hostAddress}/visbol.js" charset="utf-8"></script>
       <div>
          ${properties.svg}
       </div>
-      `
+      `;
 
-      return populateTemplate(content);
-   }
-}
+    return populateTemplate(content);
+  },
+};
 
 // template html to send
-const populateTemplate = content =>
-`
+const populateTemplate = (content) =>
+  `
 <!doctype html>
 <html>
    <head><title>VisBOL</title></head>
